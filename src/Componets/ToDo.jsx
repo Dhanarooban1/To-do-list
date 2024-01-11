3f5e93cab5e788044b79ff96a1d7e60353b54f79
Dhanarooban1 <dhanazx1@gmail.com>
2024-01-11 18:35:34 +0530

    done

--- src/Componets/ToDo.jsx
+++ src/Componets/ToDo.jsx
@@ -1,37 +1,95 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
+class ToDo extends Component {
+  constructor(props) {
+    super(props);
 
-export default class ToDo extends Component {
- constructor(props) {
- super(props)
-   this.state = {
-       todo: [],
-     inputText:""
-   }
- }
+    this.state = {
+      todos: [],
+      inputText: "",
+    };
 
-handleInputText = (e) =>{
- this.setState({
-   inputText : e.target.value
-})
-}
+    //   this.handleInputChange = this.handleInputChange.bind(this)
+  }
 
-handleData = () =>{
+  handleInputChange = (e) => {
     this.setState({
-        todo:[...this.state.todo,this.state.inputText],  
-        inputText: ""
-    })
-}
+      inputText: e.target.value,
+    });
+  };
+
+  handleSubmit = () => {
+    if (this.state.inputText === "") {
+      return;
+    }
+
+    this.setState({
+      todos: [...this.state.todos, this.state.inputText],
+      inputText: "",
+    });
+  };
+
+  handleUpdate = (idx, text) => {
+    let newArr = this.state.todos.map((ele, index) => {
+      if (index === idx) {
+        return text;
+      }
+      return ele;
+    });
+
+    this.setState({
+      todos: newArr,
+    });
+  };
+
+  handleDelete = (idx) => {
+    let newArr = this.state.todos.filter((ele, index) => {
+      if (index !== idx) {
+        return ele;
+      }
+    });
+
+    this.setState({
+      todos: newArr,
+    });
+  };
+
   render() {
-return (
-    <>
-    <div>
-       <input type="text" value ={this.state.inputText} onChange={this.handleInputText}/>
-       <button onClick={this.handleData}>Add items</button>
-    </div>
-    <div>
-        {this.state.todo.map((ele,idx)=>{
-         return  <Element key ={idx} ele = {ele}/>})}
-   </div>
-  </>)}
+    return (
+      <div id="mo">
+        <div>
+          <input
+            type="text"
+            value={this.state.inputText}
+            onChange={this.handleInputChange}
+          />
+          <button onClick={this.handleSubmit}>ADD ITEM</button>
+        </div>
+
+        <div>
+          {this.state.todos.map((ele, idx) => {
+            return (
+              <div>
+                <input
+                  className="inputElement"
+                  type="text"
+                  value={ele}
+                  onChange={(e) => this.handleUpdate(idx, e.target.value)}
+                />
+                <button
+                  onClick={() => {
+                    this.handleDelete(idx);
+                  }}
+                >
+                  Delete Item
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
+
+export default ToDo;
